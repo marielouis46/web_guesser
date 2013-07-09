6dc3f6b2a88c4253177aee1b2c93def7d5807a05
Marie Louis <marielouis46@yahoo.com>
2013-07-09 16:12:22 -0400

    If..Then done

--- web_games.rb
+++ web_games.rb
@@ -4,6 +4,11 @@ require 'sinatra/reloader'
 number = (1 + rand(99)).to_s
 
 get "/" do
+  input = params["guess"]
   erb :index, :locals => {:number => number}
 end
 
+if number.to_i > 50
+	puts "You have guessed to high"
+	erb :index, :locals => {:number => number}
+end
